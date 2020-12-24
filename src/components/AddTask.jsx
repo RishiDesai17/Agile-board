7dacfb0077fa95e4bf3081bbcbca74010f3f6ce8
Rishi N Desai <rishindesai@yahoo.com>
2020-12-24 22:31:34 +0530

    add a task

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { db, firebase } from '../firebase';
+import ActivityIndicator from './ActivityIndicator';
+import { Modal } from "react-responsive-modal";
+import "react-responsive-modal/styles.css";
+import './styles/AddTask.css';
+
+const AddTask = ({ setSections }) => {
+    const [task, setTask] = useState("")
+    const [category, setCategory] = useState("")
+    const [createdBy, setCreatedBy] = useState("")
+    const [showModal, setShowModal] = useState(false)
+    const [submitting, setSubmitting] = useState(false)
+
+    const submit = async e => {
+        try {
+            e.preventDefault()
+            if(task === "" || category === "" || createdBy === "") {
+                alert("Fill in all the fields")
+                return
+            }
+            setSubmitting(true)
+            const response = await db.collection("projects/kGS550UTeB1nYSQBYzPf/assigned").add({
+                task,
+                category,
+                createdBy,
+                createdAt: firebase.firestore.FieldValue.serverTimestamp()
+            })
+            const newTask = await response.get()
+            setSections(sections => {
+                return {
+                    ...sections,
+                    assigned: [newTask, ...sections.assigned]
+                }
+            })
+            setShowModal(false)
+            setSubmitting(false)
+        }
+        catch(err) {
+            console.log(err)
+        }
+    }
+
+    return (
+        <div>
+            <button onClick={() => setShowModal(true)}>+ Add Task</button>
+            <Modal open={showModal} onClose={() => setShowModal(false)}>
+                <form onSubmit={submit}>
+                    <div>
+                        <input placeholder="Task"  onChange={e => setTask(e.target.value)} />
+                    </div>
+                    <div>
+                        <input placeholder="Category"  onChange={e => setCategory(e.target.value)} />
+                    </div>
+                    <div>
+                        <input placeholder="Created by" onChange={e => setCreatedBy(e.target.value)}  />
+                    </div>
+                    {submitting ? 
+                        <ActivityIndicator />
+                    :
+                        <div>
+                            <button type="submit" id="submitButton" disabled={submitting}>submit</button>
+                        </div>
+                    }
+                </form>
+            </Modal>
+        </div>
+    )
+}
+
+export default AddTask
